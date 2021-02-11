f412db9a9e86beeb8695172aed11d3ef44df68e8
Simen Kristoffersen <simekri@stud.ntnu.no>
2021-02-11 13:06:03 +0100

    Initial commit for frontend
    - Satt opp frontend med linters for kode og .scss

---
+++ frontend/config/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ident: 'postcss',
+  plugins: [
+    require('postcss-preset-env')({
+      browsers: 'last 2 versions',
+    }),
+  ],
+};
---
+++ frontend/config/webpack.dev.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+
+/**
+ * Webpack configuration for development environment
+ * @module webpack.dev
+ */
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 3000,
+    proxy: {},
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), new webpack.NoEmitOnErrorsPlugin()],
+});

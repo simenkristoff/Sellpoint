fc6352f4b4cfd4d4692b9cd1fceaba273c85434c
andrinbe <andrinbe@stud.ntnu.no>
2021-02-14 12:06:36 +0100

    Add logo to page header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Layout, Menu} from 'antd'
+import {Layout, Menu, Image} from 'antd'
 import {Link} from 'react-router-dom'
 
 
@@ -7,6 +7,7 @@ export const Header: React.FC = () => {
     return (
         <Layout.Header className='site-header'>
             <Menu mode='horizontal' className='main-nav'>
+                <img src='..\..\assets\img\logo.png' height='60' width='55' style={{paddingBottom:5}}></img>
                 <Menu.Item>
                     <Link to='/'>Hjem</Link>
                 </Menu.Item>

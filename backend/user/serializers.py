96d1aece8b218be2ecd65dca449ac6abea8feee2
Dag <dag.b.okland@gmail.com>
2021-02-23 13:00:02 +0100

    fix merge oopsie

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'is_superuser', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email', 'is_superuser', 'first_name', 'last_name')
 
 
 class RegisterSerializer(serializers.ModelSerializer):
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -3,8 +3,16 @@ from user.views import RegisterView
 from rest_framework_jwt.views import obtain_jwt_token
 from .views import UserViewSet, RegisterView
 
+user_list = UserViewSet.as_view({
+    'get': 'list'
+}) 
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 urlpatterns = [
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail'),
     path('login/', obtain_jwt_token),
     path('register/', RegisterView.as_view(), name='user_register'),
 ]
